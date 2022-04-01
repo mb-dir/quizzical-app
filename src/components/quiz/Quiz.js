c2b17ff02a14664ac6abb6e97769a158d61c14a5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-01 17:13:47 +0200

    Styles for Quiz component

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,12 +1,19 @@
+import "./Quiz.css";
 export default function Quiz() {
   return (
     <main className="quiz">
       <section className="question quiz__question">
         <p className="question__content">Extremely hard question</p>
         <ul className="question__answers">
-          <li className="question__answer">Ansewr 1</li>
-          <li className="question__answer">Ansewr 2</li>
-          <li className="question__answer">Ansewr 3</li>
+          <li className="question__answer question__answer--checked">
+            Ansewr 1
+          </li>
+          <li className="question__answer question__answer--correct">
+            Ansewr 2
+          </li>
+          <li className="question__answer question__answer--incorrect">
+            Ansewr 3
+          </li>
           <li className="question__answer">Ansewr 4</li>
           <li className="question__answer">Ansewr 5</li>
         </ul>
