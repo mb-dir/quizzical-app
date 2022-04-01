ae68983a11af9bf80c05d7577433c6cfb5efb007
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-01 17:43:02 +0200

    State determining if game had started
    Next step: pass the special function (which will change the isStart value) to Start component - when user clicks the btn, the state will change and
    thanks conditional rendering the Quiz component will be dispplayed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import Start from "./components/start/Start";
 import Quiz from "./components/quiz/Quiz";
 
 function App() {
+  const [ isStart, setIsStart ] = React.useState(false);
   return (
     <div className="App">
       {/* There will be a condition rendering - when user click "Start quiz" btn the view will change to quiz section */}
-      {/* <Start /> */}
-      <Quiz />
+      {isStart ? <Quiz /> : <Start />}
     </div>
   );
 }
