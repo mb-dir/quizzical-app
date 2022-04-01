ee8f35508015c34ac70b162b8be0e04120415486
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-01 16:55:29 +0200

    Basic content for Quiz component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Start from "./components/start/Start";
+import Quiz from "./components/quiz/Quiz";
 
 function App() {
   return (
     <div className="App">
       {/* There will be a condition rendering - when user click "Start quiz" btn the view will change to quiz section */}
-      <Start />
+      {/* <Start /> */}
+      <Quiz />
     </div>
   );
 }
---
+++ src/components/quiz/Quiz.js
@@ -0,0 +1,28 @@
+export default function Quiz() {
+  return (
+    <main className="quiz">
+      <section className="question quiz__question">
+        <p className="question__content">Extremely hard question</p>
+        <ul className="question__answers">
+          <li className="question__answer">Ansewr 1</li>
+          <li className="question__answer">Ansewr 2</li>
+          <li className="question__answer">Ansewr 3</li>
+          <li className="question__answer">Ansewr 4</li>
+          <li className="question__answer">Ansewr 5</li>
+        </ul>
+      </section>
+
+      <section className="question quiz__question">
+        <p className="question__content">Another Extremely hard question</p>
+        <ul className="question__answers">
+          <li className="question__answer">Ansewr 1</li>
+          <li className="question__answer">Ansewr 2</li>
+          <li className="question__answer">Ansewr 3</li>
+          <li className="question__answer">Ansewr 4</li>
+          <li className="question__answer">Ansewr 5</li>
+        </ul>
+      </section>
+      <button className="quiz__showAnswers">Show answers</button>
+    </main>
+  );
+}
