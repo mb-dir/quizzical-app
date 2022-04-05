9d7805cdd3874f1c1498f66f0aa4b92817ded7fc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-05 21:50:41 +0200

    Refactor the questions state structure
    Now in questions state there are all requred informations, questions
    content and everything related with answers(answers content, info if
    answer is checked, and if it is correct). You can find an example
    structure in line 5  - I know it could be a little bit confusing so I
    add an example question structure as a comment

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -2,13 +2,56 @@ import React from "react";
 import "./Quiz.css";
 export default function Quiz() {
   //The api returnes the object, but the questions are in an array so init value if state is an empty array - look here to see the structure of response: https://codepen.io/mb-dir/pen/LYeeJXM?editors=1011, or just use postman
+  //questions is an array of object, each obj describes the question(includes question content + answers, each ansewr is an separate obj keept in special array), example questions structure may look like:
+  // [{
+  //   questionContent: "Which Toy Story; character was voiced by Don Rickles?"
+  //   answerDescription: [
+  //    {
+  //      content: Slinky dog,
+  //      isChecked: false,
+  //      isCorrect: false,
+  //     },
+  //    {
+  //      content: Rex,
+  //      isChecked: false,
+  //      isCorrect: false,
+  //     }
+  //  ]
+  // }]
   const [ questions, setQuestions ] = React.useState([]);
 
   //useEfect for sync the response from fetch with state(questions)
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5")
       .then(res => res.json())
-      .then(res => setQuestions(res.results))
+      .then(res => {
+        //arrays with questions which will be set as a state
+        const questions = [];
+
+        //results is an array of obj, of each obj we have to create the questionDescription
+        res.results.forEach(questionObj => {
+          const questionDescription = {};
+          const answersDescription = [];
+          //imperative way of "extracting" the required data
+          const { correct_answer, incorrect_answers, question } = questionObj;
+          const possibilityAnswers = [ ...incorrect_answers ];
+          possibilityAnswers.push(correct_answer);
+
+          possibilityAnswers.forEach(answer => {
+            const answerDescription = {
+              content: answer,
+              isChecked: false,
+              isCorrect: answer === correct_answer ? true : false,
+            };
+            answersDescription.push(answerDescription);
+          });
+          questionDescription.questionContent = question;
+          questionDescription.answerDescription = answersDescription;
+          questions.push(questionDescription);
+        });
+
+        setQuestions(questions);
+      })
       .catch(err => console.log(err));
   }, []);
 
